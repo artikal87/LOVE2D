26fc0ccc5c31ddcd6e5ca7e61d40dc7bfdbe9d22
artikal87 <ptracey.87@gmail.com>
2025-04-15 23:40:46 +0100

    added selection sort

--- Py/Algos.py
+++ Py/Algos.py
@@ -12,5 +12,21 @@ def binary_search(arr,item):
             low = mid + 1   # therefore, the lowest guess index becomes 1 higher than the mid. 
     return None
 
-def second_function():
-    return None
+def find_smallest(arr):
+    smallest = arr[0]                   # smallest is index 0 to start with
+    smallest_index = 0                  # so is the return variable
+    for i in range(1,len(arr)):         # loop across whole array
+        if arr[i] < smallest:           # if the current index's value is smaller than smallest
+            smallest = arr[i]           # make that value the smallest
+            smallest_index = i          # do the same for the return variable
+    return smallest_index
+
+def selection_sort(arr):
+    newArr = []
+    copiedArr = list(arr)
+    for i in range(len(copiedArr)):
+        smallest = find_smallest(copiedArr)     # find the smallest value in the copied array list
+        newArr.append(copiedArr.pop(smallest))  # append the smallest term to new array. Done using pop method 
+    return newArr                               # which removes & returns value at specified index
+    
+    
